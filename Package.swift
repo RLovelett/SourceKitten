00e9293eb9621f75f0a98625726a5bbb503c80ad
Ryan Lovelett <ryan@lovelett.me>
2016-11-16 21:39:14 -0500

    Use my Clang_C patch

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     .Package(url: "https://github.com/Carthage/Commandant.git", versions: Version(0, 11, 3)..<Version(0, 11, .max)),
     .Package(url: "https://github.com/drmohundro/SWXMLHash.git", Version(3, 0, 2)),
     .Package(url: "https://github.com/behrang/YamlSwift.git", Version(3, 1, 0)),
-    .Package(url: "https://github.com/norio-nomura/Clang_C.git", majorVersion: 1),
+    .Package(url: "https://github.com/RLovelett/Clang_C.git", majorVersion: 1),
     .Package(url: "https://github.com/norio-nomura/SourceKit.git", majorVersion: 1),
   ],
   exclude: ["Tests/SourceKittenFrameworkTests/Fixtures"]
